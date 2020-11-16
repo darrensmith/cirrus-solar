6cc82a95eeffc4fe6bec20ac3547865dda005a38
Darren Smith <contact@darrensmith.com.au>
2020-11-16 13:20:23 +1100

    changes

--- services/cirrus-solar/controllers/app/admin/controller.js
+++ services/cirrus-solar/controllers/app/admin/controller.js
@@ -10,6 +10,27 @@
 	var ctrl = {};
 
 	/**
+	 * INIT
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.init = function(core){
+		var context = {};
+		var httpClient = core.module("http", "interface").client;
+		var service = core.module("services").service("cirrus-solar");
+		setInterval(function(){
+			httpClient.get("http://solar.home.darrensmith.com.au", function(httpErr, httpRes) {
+				if(httpRes) {
+					log("debug", "Cirrus Solar Application > Polled Solar Panel and Got Voltage of " + (httpRes.data.voltage / 100));
+					context.voltage = service.vars.set("voltage", (httpRes.data.voltage / 100));
+				} else {
+					log("debug", "Cirrus Solar Application > Error Polling Solar Panel", httpErr);
+				}
+			});
+		}, 1000);
+	}
+
+	/**
 	 * GET
 	 * @param {object} req - Request object
 	 * @param {object} res - Response object
